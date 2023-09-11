615eaec3a3c14af3cd84f50b0b48c1e442e69d2a
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-09-11 15:32:11 +0300

    UserController

--- src/main/java/falusvampen/letsplay/controller/UserController.java
+++ src/main/java/falusvampen/letsplay/controller/UserController.java
@@ -1,10 +1,12 @@
 package falusvampen.letsplay.controller;
 
+import falusvampen.letsplay.models.User;
+import falusvampen.letsplay.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import falusvampen.letsplay.models.User;
-import falusvampen.letsplay.services.UserService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users") // The base URL for this controller
@@ -16,9 +18,49 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping // This method handles POST requests to create a User
+    // Create a User
+    @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
         User createdUser = userService.createUser(user);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    // Read All Users
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    // Read User by Id
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable String id) {
+        User user = userService.getUserById(id);
+        if (user != null) {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // Update User
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable String id, @RequestBody User updatedUser) {
+        User user = userService.updateUser(id, updatedUser);
+        if (user != null) {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // Delete User
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable String id) {
+        if (userService.deleteUser(id)) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
