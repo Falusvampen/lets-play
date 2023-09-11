5d89c09b7ac520be60ec5835449f245279554cae
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-09-11 19:34:21 +0300

    CRUD endpoints made

--- src/main/java/falusvampen/letsplay/controller/ProductController.java
+++ src/main/java/falusvampen/letsplay/controller/ProductController.java
@@ -1,19 +1,63 @@
-// package falusvampen.letsplay.controller;
+package falusvampen.letsplay.controller;
 
-// import falusvampen.letsplay.models.User;
-// import falusvampen.letsplay.services.ProductService;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.*;
+import falusvampen.letsplay.models.Product;
+import falusvampen.letsplay.services.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-// @RestController
-// @RequestMapping("/api/products") // The base URL for this controller
-// public class ProductController {
+@RestController
+@RequestMapping("/api/products") // The base URL for this controller
+public class ProductController {
 
-// private final ProductService productService;
+    private final ProductService productService;
 
-// public ProductController(ProductService productService) {
-// this.productService = productService;
-// }
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 
-// }
+    // Create a Product
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        Product createdProduct = productService.createProduct(product);
+        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
+    }
+
+    // Read All Products
+    @GetMapping
+    public ResponseEntity<Iterable<Product>> getAllProducts() {
+        Iterable<Product> products = productService.getAllProducts();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    // Read Product by Id
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable String id) {
+        Product product = productService.getProductById(id);
+        if (product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    // Update Product
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable String id, @RequestBody Product updatedProduct) {
+        Product product = productService.updateProduct(id, updatedProduct);
+        if (product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    // Delete Product
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteProduct(@PathVariable String id) {
+        boolean deleted = productService.deleteProduct(id);
+        if (deleted) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+}
