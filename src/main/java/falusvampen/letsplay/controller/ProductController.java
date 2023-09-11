0c6040b4ff8057078c85608705f9797f68a97afd
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-09-11 15:32:03 +0300

    started working on productController

---
+++ src/main/java/falusvampen/letsplay/controller/ProductController.java
@@ -0,0 +1,19 @@
+// package falusvampen.letsplay.controller;
+
+// import falusvampen.letsplay.models.User;
+// import falusvampen.letsplay.services.ProductService;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
+
+// @RestController
+// @RequestMapping("/api/products") // The base URL for this controller
+// public class ProductController {
+
+// private final ProductService productService;
+
+// public ProductController(ProductService productService) {
+// this.productService = productService;
+// }
+
+// }
