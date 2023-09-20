42e09f5da9176ed5f5ba4b5d5a55e72ea1cc9ccc
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-09-20 16:23:22 +0300

    cleaned code

--- src/main/java/falusvampen/letsplay/services/DatabaseSeeder.java
+++ src/main/java/falusvampen/letsplay/services/DatabaseSeeder.java
@@ -1,34 +1,5 @@
 package falusvampen.letsplay.services;
 
-// import falusvampen.letsplay.models.User;
-// import falusvampen.letsplay.repositories.UserRepository;
-// import falusvampen.letsplay.repositories.ProductRepository;
-// import org.springframework.boot.CommandLineRunner;
-// import org.springframework.stereotype.Component;
-
-// @Component
-// public class DatabaseSeeder implements CommandLineRunner {
-
-// private final UserRepository userRepository;
-// private final ProductRepository productRepository;
-
-// public DatabaseSeeder(UserRepository userRepository, ProductRepository
-// productRepository) {
-// this.userRepository = userRepository;
-// this.productRepository = productRepository;
-// }
-
-// @Override
-// public void run(String... args) {
-// System.out.println("Fetching all users from database...");
-// userRepository.findAll().forEach(user -> System.out.println(user.getName()));
-// System.out.println("Fetching all products from database...");
-// productRepository.findAll().forEach(product ->
-// System.out.println(product.getName()));
-
-// }
-// }
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import falusvampen.letsplay.models.User;
