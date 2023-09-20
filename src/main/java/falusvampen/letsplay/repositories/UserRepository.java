750890a42da696aadb3753cb34888e99e8792e0c
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-09-20 18:56:15 +0300

    added findbyuser

--- src/main/java/falusvampen/letsplay/repositories/UserRepository.java
+++ src/main/java/falusvampen/letsplay/repositories/UserRepository.java
@@ -2,6 +2,12 @@ package falusvampen.letsplay.repositories;
 
 import falusvampen.letsplay.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
+    @Query("{'email': ?0}")
+    Optional<User> findByUser(String email);
+
 }
