42db5c7fc4758780b035041b5bdda4df181996ff
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-09-20 18:56:31 +0300

    exceptions for users

---
+++ src/main/java/falusvampen/letsplay/exceptions/UserCollectionException.java
@@ -0,0 +1,40 @@
+package falusvampen.letsplay.exceptions;
+
+import java.io.Serial;
+
+public class UserCollectionException extends Exception {
+    /**
+     *
+     */
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public UserCollectionException(String message) {
+        super(message);
+    }
+
+    public static String NotFoundException(String id) {
+        return "User with " + id + " not found!";
+    }
+
+    public static String UserAlreadyExistException() {
+        return "User already exist!";
+    }
+
+    public static String NullException() {
+        return " cannot be empty!";
+    }
+
+    public static String InvalidEmailException() {
+        return "Invalid email format!";
+    }
+
+    public static String InvalidRoleException() {
+        return " has to be in either ROLE_ADMIN or ROLE_USER format!";
+    }
+
+    public static String BadCredentialsException() {
+        return "Invalid password format!";
+    }
+
+}
