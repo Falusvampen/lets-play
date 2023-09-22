26fdc93d9ee629e78a924e061512781547f253b7
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-09-22 13:19:14 +0300

    finished jwt filter

--- src/main/java/falusvampen/letsplay/filter/JWTFilter.java
+++ src/main/java/falusvampen/letsplay/filter/JWTFilter.java
@@ -2,9 +2,20 @@ package falusvampen.letsplay.filter;
 
 import falusvampen.letsplay.service.JWTService;
 import falusvampen.letsplay.service.UserInfoDetailsService;
-
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+
+import java.io.IOException;
 
 public class JWTFilter extends OncePerRequestFilter {
 
@@ -14,4 +25,39 @@ public class JWTFilter extends OncePerRequestFilter {
     @Autowired
     private UserInfoDetailsService userInfoDetailsService;
 
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String jwt = authHeader.substring(7); // Remove "Bearer "
+
+            // Extract the username
+            String username = jwtService.extractUsername(jwt);
+
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails;
+                try {
+                    userDetails = userInfoDetailsService.loadUserByUsername(username);
+                } catch (UsernameNotFoundException e) {
+                    filterChain.doFilter(request, response);
+                    return;
+                }
+
+                // Validate the JWT against the loaded UserDetails
+                if (jwtService.validateToken(jwt, userDetails)) {
+                    // Create Authentication Token
+                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                            userDetails, null, userDetails.getAuthorities());
+
+                    // Populate Security Context
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
