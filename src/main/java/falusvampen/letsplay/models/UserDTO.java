de87eafe32b5cbbe4d63cbed97ba15eb1c65d034
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-09-24 23:12:36 +0300

    Add UserDTO model class
    This commit adds the UserDTO model class which includes the id, name, and role fields. It also includes the necessary Lombok annotations for generating the getter, setter, constructor, and toString methods.

---
+++ src/main/java/falusvampen/letsplay/models/UserDTO.java
@@ -0,0 +1,15 @@
+package falusvampen.letsplay.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private String id;
+    private String name;
+    private String role;
+
+}
