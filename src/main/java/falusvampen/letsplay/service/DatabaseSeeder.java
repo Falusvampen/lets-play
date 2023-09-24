f1e002bcd2d64f7774fdccbbe3ba35d0ff096057
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-09-24 22:29:48 +0300

    Refactor user and product creation in DatabaseSeeder
    - Change user credentials from specific names to generic ones
    - Update user and product creation with new credentials
    - Associate user ID with respective products

--- src/main/java/falusvampen/letsplay/service/DatabaseSeeder.java
+++ src/main/java/falusvampen/letsplay/service/DatabaseSeeder.java
@@ -44,16 +44,16 @@ public class DatabaseSeeder implements CommandLineRunner {
         // You can create and save User objects here
         User user1 = new User();
         user1.setId("1");
-        user1.setName("JohnDoe");
-        user1.setEmail("johndoe@example.com");
-        user1.setPassword(passwordEncoder.encode("password123"));
+        user1.setName("user");
+        user1.setEmail("user@example.com");
+        user1.setPassword(passwordEncoder.encode("password"));
         user1.setRole("ROLE_USER");
 
         User user2 = new User();
         user2.setId("2");
-        user2.setName("falusvampen");
-        user2.setEmail("falusvampen@example.com");
-        user2.setPassword(passwordEncoder.encode("nice"));
+        user2.setName("admin");
+        user2.setEmail("admin@example.com");
+        user2.setPassword(passwordEncoder.encode("password"));
         user2.setRole("ROLE_ADMIN");
 
         // Save the User objects to the database
@@ -70,12 +70,14 @@ public class DatabaseSeeder implements CommandLineRunner {
         product1.setName("Product 1");
         product1.setPrice(100.0);
         product1.setDescription("This is product 1");
+        product1.setUserid("1");
 
         Product product2 = new Product();
         product2.setProductid("2");
         product2.setName("Product 2");
         product2.setPrice(200.0);
         product2.setDescription("This is product 2");
+        product2.setUserid("2");
 
         productRepository.save(product1);
         productRepository.save(product2);
