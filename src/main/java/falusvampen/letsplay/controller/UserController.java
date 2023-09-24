5b470fa8f30021b3e674dcb8ac292ec1c9344b22
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-09-24 23:12:20 +0300

    refactor UserController to use UserDTO, add authorization to methods

--- src/main/java/falusvampen/letsplay/controller/UserController.java
+++ src/main/java/falusvampen/letsplay/controller/UserController.java
@@ -1,9 +1,11 @@
 package falusvampen.letsplay.controller;
 
 import falusvampen.letsplay.models.User;
+import falusvampen.letsplay.models.UserDTO;
 import falusvampen.letsplay.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,6 +22,7 @@ public class UserController {
 
     // Create a User
     @PostMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<User> createUser(@RequestBody User user) {
         User createdUser = userService.createUser(user);
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
@@ -27,17 +30,19 @@ public class UserController {
 
     // Read All Users
     @GetMapping
-    public ResponseEntity<List<User>> getAllUsers() {
-        List<User> users = userService.getAllUsers();
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        List<UserDTO> users = userService.getAllUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
     // Read User by Id
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable String id) {
-        User user = userService.getUserById(id);
-        if (user != null) {
-            return new ResponseEntity<>(user, HttpStatus.OK);
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable String id) {
+        UserDTO userDTO = userService.getUserById(id);
+        if (userDTO != null) {
+            return new ResponseEntity<>(userDTO, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
@@ -45,6 +50,7 @@ public class UserController {
 
     // Update User
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<User> updateUser(@PathVariable String id, @RequestBody User updatedUser) {
         User user = userService.updateUser(id, updatedUser);
         if (user != null) {
@@ -56,6 +62,7 @@ public class UserController {
 
     // Delete User
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<HttpStatus> deleteUser(@PathVariable String id) {
         if (userService.deleteUser(id)) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
