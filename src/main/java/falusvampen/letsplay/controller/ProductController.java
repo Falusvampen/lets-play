391398bb85232504d05202b7bd4ef99d46c6d538
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-09-25 17:58:44 +0300

    Validate user input to prevent MongoDB injection attacks
    - Added a utility method `isValidInput` in `ProductController` to validate user input against MongoDB injection attacks.
    - The method checks for the presence of special characters like `$`, `{`, and `}` in the input.
    - Updated the code in `createProduct`, `getProductById`, `updateProduct`, and `deleteProduct` methods to invoke `isValidInput` for input validation.
    - If the input is invalid, a "Bad Request" response with an error message is returned.
    
    [More detailed description can be added here if necessary]

--- src/main/java/falusvampen/letsplay/controller/ProductController.java
+++ src/main/java/falusvampen/letsplay/controller/ProductController.java
@@ -17,17 +17,31 @@ public class ProductController {
         this.productService = productService;
     }
 
+    // Utility to validate input against MongoDB injection attacks
+    private boolean isValidInput(String input) {
+        if (input == null || input.isEmpty()) {
+            return false;
+        }
+
+        // Check for MongoDB special characters
+        if (input.contains("$") || input.contains("{") || input.contains("}")) {
+            return false;
+        }
+
+        return true;
+    }
+
     @PostMapping
-    @PreAuthorize("hasRole('ROLE_USER')")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     public ResponseEntity<?> createProduct(@RequestBody Product product) {
         try {
-            if (product == null) {
+            if (product == null || !isValidInput(product.getName())) { // Added validation
                 return new ResponseEntity<>("Invalid input", HttpStatus.BAD_REQUEST);
             }
             Product createdProduct = productService.createProduct(product);
             return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
         } catch (Exception e) {
-            return new ResponseEntity<>("Internal Server Error", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("An error occurred", HttpStatus.BAD_REQUEST);
         }
     }
 
@@ -37,28 +51,32 @@ public class ProductController {
             Iterable<Product> products = productService.getAllProducts();
             return new ResponseEntity<>(products, HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>("Internal Server Error", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("An error occurred", HttpStatus.BAD_REQUEST);
         }
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getProductById(@PathVariable String id) {
         try {
+            if (!isValidInput(id)) { // Added validation
+                return new ResponseEntity<>("Invalid input", HttpStatus.BAD_REQUEST);
+            }
             Product product = productService.getProductById(id);
             if (product != null) {
                 return new ResponseEntity<>(product, HttpStatus.OK);
             }
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } catch (Exception e) {
-            return new ResponseEntity<>("Internal Server Error", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("An error occurred", HttpStatus.BAD_REQUEST);
         }
     }
 
     @PutMapping("/{id}")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     public ResponseEntity<?> updateProduct(@PathVariable String id, @RequestBody Product updatedProduct) {
         try {
-            if (updatedProduct == null) {
+            if (updatedProduct == null || !isValidInput(id) || !isValidInput(updatedProduct.getName())) { // Added
+                                                                                                          // validation
                 return new ResponseEntity<>("Invalid input", HttpStatus.BAD_REQUEST);
             }
             Product product = productService.updateProduct(id, updatedProduct);
@@ -67,7 +85,7 @@ public class ProductController {
             }
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } catch (Exception e) {
-            return new ResponseEntity<>("Internal Server Error", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("An error occurred", HttpStatus.BAD_REQUEST);
         }
     }
 
@@ -75,13 +93,16 @@ public class ProductController {
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<?> deleteProduct(@PathVariable String id) {
         try {
+            if (!isValidInput(id)) { // Added validation
+                return new ResponseEntity<>("Invalid input", HttpStatus.BAD_REQUEST);
+            }
             boolean deleted = productService.deleteProduct(id);
             if (deleted) {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } catch (Exception e) {
-            return new ResponseEntity<>("Internal Server Error", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("An error occurred", HttpStatus.BAD_REQUEST);
         }
     }
 }
--- src/main/java/falusvampen/letsplay/repositories/ProductRepository.java
+++ src/main/java/falusvampen/letsplay/repositories/ProductRepository.java
@@ -1,7 +1,17 @@
 package falusvampen.letsplay.repositories;
 
 import falusvampen.letsplay.models.Product;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface ProductRepository extends MongoRepository<Product, String> {
+    @Query("{'id': ?0}")
+    Optional<Product> findByProduct(Product product);
+
+    @Query("{'userId': ?0}")
+    List<Product> findByUserId(String userId);
 }
