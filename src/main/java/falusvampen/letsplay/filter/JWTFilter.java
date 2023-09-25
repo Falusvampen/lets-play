061782ae949ad86ae6d46f80ae8d561c3caabf60
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-09-25 17:59:14 +0300

    [filter] Improve JWT handling and error responses
    - Refactored the `doFilterInternal` method in `JWTFilter` to improve code readability and maintainability.
    - Added a new `handleRequest` method to handle the logic for processing the JWT token.
    - Created a new `InvalidTokenException` subclass of `RuntimeException` to handle invalid tokens.
    - Added a `handleException` method to handle exceptions and send appropriate error responses.
    - Updated the logic for handling the JWT token, including extracting the username, validating the token, and populating the security context.
    - Implemented error handling for different types of exceptions, sending appropriate HTTP error responses.

--- src/main/java/falusvampen/letsplay/filter/JWTFilter.java
+++ src/main/java/falusvampen/letsplay/filter/JWTFilter.java
@@ -2,20 +2,19 @@ package falusvampen.letsplay.filter;
 
 import falusvampen.letsplay.service.JWTService;
 import falusvampen.letsplay.service.UserInfoDetailsService;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
-
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-
 import java.io.IOException;
 
 @Component
@@ -27,39 +26,72 @@ public class JWTFilter extends OncePerRequestFilter {
     @Autowired
     private UserInfoDetailsService userInfoDetailsService;
 
+    public static class InvalidTokenException extends RuntimeException {
+        public InvalidTokenException(String message) {
+            super(message);
+        }
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        try {
+            handleRequest(request, response, filterChain);
+        } catch (Exception e) {
+            handleException(response, e);
+        }
+    }
+
+    private void handleRequest(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
         String authHeader = request.getHeader("Authorization");
 
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+        if (isBearerToken(authHeader)) {
             String jwt = authHeader.substring(7); // Remove "Bearer "
-
-            // Extract the username
-            String username = jwtService.extractUsername(jwt);
-
-            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                UserDetails userDetails;
-                try {
-                    userDetails = userInfoDetailsService.loadUserByUsername(username);
-                } catch (UsernameNotFoundException e) {
-                    filterChain.doFilter(request, response);
-                    return;
-                }
-
-                // Validate the JWT against the loaded UserDetails
-                if (jwtService.validateToken(jwt, userDetails)) {
-                    // Create Authentication Token
-                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                            userDetails, null, userDetails.getAuthorities());
-
-                    // Populate Security Context
-                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContextHolder.getContext().setAuthentication(authentication);
-                }
-            }
+            handleBearerToken(jwt, request);
         }
 
         filterChain.doFilter(request, response);
+    }
+
+    private boolean isBearerToken(String authHeader) {
+        return authHeader != null && authHeader.startsWith("Bearer ");
+    }
+
+    private void handleBearerToken(String jwt, HttpServletRequest request) {
+        String username = jwtService.extractUsername(jwt);
+        if (username == null || SecurityContextHolder.getContext().getAuthentication() != null) {
+            return;
+        }
+
+        UserDetails userDetails = getUserDetailsOrThrow(username);
+        if (!jwtService.validateToken(jwt, userDetails)) {
+            throw new InvalidTokenException("Invalid token");
+        }
+
+        populateSecurityContext(userDetails, request);
+    }
+
+    private UserDetails getUserDetailsOrThrow(String username) {
+        try {
+            return userInfoDetailsService.loadUserByUsername(username);
+        } catch (UsernameNotFoundException e) {
+            throw new IllegalStateException("User not found", e);
+        }
+    }
+
+    private void populateSecurityContext(UserDetails userDetails, HttpServletRequest request) {
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                userDetails, null, userDetails.getAuthorities());
+        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private void handleException(HttpServletResponse response, Exception e) throws IOException {
+        if (e instanceof IllegalStateException || e instanceof InvalidTokenException) {
+            response.sendError(HttpStatus.UNAUTHORIZED.value(), e.getMessage());
+        } else {
+            response.sendError(HttpStatus.BAD_REQUEST.value(), "An error occurred");
+        }
     }
 }
