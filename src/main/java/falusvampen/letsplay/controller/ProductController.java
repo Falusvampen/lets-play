5ba0d8d70a1ee1a057a9127be691c40ef39b75b1
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-09-25 15:41:51 +0300

    Refactor code in ProductController for CRUD operations
    - Refactor the code in ProductController to improve error handling and response formatting for CRUD operations.
    - Add try-catch blocks to handle exceptions and return appropriate response entities with error messages.
    - Improve input validation by checking for null values and returning a bad request response entity.
    - Update the createProduct, getAllProducts, getProductById, updateProduct, and deleteProduct methods accordingly.

--- src/main/java/falusvampen/letsplay/controller/ProductController.java
+++ src/main/java/falusvampen/letsplay/controller/ProductController.java
@@ -8,7 +8,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/products") // The base URL for this controller
+@RequestMapping("/api/products")
 public class ProductController {
 
     private final ProductService productService;
@@ -17,51 +17,71 @@ public class ProductController {
         this.productService = productService;
     }
 
-    // Create a Product at the URL /api/products
     @PostMapping
     @PreAuthorize("hasRole('ROLE_USER')")
-    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
-        Product createdProduct = productService.createProduct(product);
-        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
-    }
-
-    // Read All Products
-    @GetMapping
-    public ResponseEntity<Iterable<Product>> getAllProducts() {
-        Iterable<Product> products = productService.getAllProducts();
-        return new ResponseEntity<>(products, HttpStatus.OK);
-    }
-
-    // Read Product by Id
-    @GetMapping("/{id}")
-    public ResponseEntity<Product> getProductById(@PathVariable String id) {
-        Product product = productService.getProductById(id);
-        if (product != null) {
-            return new ResponseEntity<>(product, HttpStatus.OK);
+    public ResponseEntity<?> createProduct(@RequestBody Product product) {
+        try {
+            if (product == null) {
+                return new ResponseEntity<>("Invalid input", HttpStatus.BAD_REQUEST);
+            }
+            Product createdProduct = productService.createProduct(product);
+            return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Internal Server Error", HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    // Update Product by Id at the URL /api/products/{id}
+    @GetMapping
+    public ResponseEntity<?> getAllProducts() {
+        try {
+            Iterable<Product> products = productService.getAllProducts();
+            return new ResponseEntity<>(products, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Internal Server Error", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getProductById(@PathVariable String id) {
+        try {
+            Product product = productService.getProductById(id);
+            if (product != null) {
+                return new ResponseEntity<>(product, HttpStatus.OK);
+            }
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Internal Server Error", HttpStatus.BAD_REQUEST);
+        }
+    }
+
     @PutMapping("/{id}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ResponseEntity<Product> updateProduct(@PathVariable String id, @RequestBody Product updatedProduct) {
-        Product product = productService.updateProduct(id, updatedProduct);
-        if (product != null) {
-            return new ResponseEntity<>(product, HttpStatus.OK);
+    public ResponseEntity<?> updateProduct(@PathVariable String id, @RequestBody Product updatedProduct) {
+        try {
+            if (updatedProduct == null) {
+                return new ResponseEntity<>("Invalid input", HttpStatus.BAD_REQUEST);
+            }
+            Product product = productService.updateProduct(id, updatedProduct);
+            if (product != null) {
+                return new ResponseEntity<>(product, HttpStatus.OK);
+            }
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Internal Server Error", HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    // Delete Product by Id at the URL /api/products/{id}
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ResponseEntity<HttpStatus> deleteProduct(@PathVariable String id) {
-        boolean deleted = productService.deleteProduct(id);
-        if (deleted) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public ResponseEntity<?> deleteProduct(@PathVariable String id) {
+        try {
+            boolean deleted = productService.deleteProduct(id);
+            if (deleted) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Internal Server Error", HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-
 }
