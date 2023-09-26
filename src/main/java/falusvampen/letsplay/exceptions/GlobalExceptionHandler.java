c06fb09beed0c79b289c5d2939c6564c40894b6c
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-09-26 18:51:19 +0300

    Add MongoDB exception handler
    - Adds a new exception handler for MongoDB exceptions in GlobalExceptionHandler.java.
    - This handler logs the exception message for internal tracking.
    - It returns a response with the message "Service temporarily unavailable" and status code 503 (SERVICE_UNAVAILABLE) when a MongoDB exception is thrown.

--- src/main/java/falusvampen/letsplay/exceptions/GlobalExceptionHandler.java
+++ src/main/java/falusvampen/letsplay/exceptions/GlobalExceptionHandler.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.mongodb.MongoException;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -19,4 +21,12 @@ public class GlobalExceptionHandler {
     // public ResponseEntity<Object> handleOtherExceptions(Exception e) {
     // return new ResponseEntity<>("An error occurred", HttpStatus.BAD_REQUEST);
     // }
+    @ExceptionHandler(MongoException.class)
+    public ResponseEntity<String> handleMongoException(MongoException e) {
+        // Log the exception for internal tracking.
+        // You can use any logging framework here.
+        System.err.println("MongoDB Error: " + e.getMessage());
+
+        return new ResponseEntity<>("Service temporarily unavailable", HttpStatus.SERVICE_UNAVAILABLE);
+    }
 }
