1c1c8c13fdfb02e10e6d3d64b160bca5bf54e3bc
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-09-26 18:51:44 +0300

    Add exception handling and validation for product creation and update
    - Adds exception handling and validation for the creation and update of products in the ProductService class.
    - Adds a new method to validate the product before saving it to the repository.
    - Updates the createProduct method to check if the product ID exists and generate a new one if it doesn't.
    - Updates the updateProduct method to check if any changes were made to the product and throws an exception if not.
    - Updates the updateProduct method to check if the user exists before updating the product.
    - Adds exception handling for the deleteProduct method to check if the product exists before deleting it.

--- src/main/java/falusvampen/letsplay/service/ProductService.java
+++ src/main/java/falusvampen/letsplay/service/ProductService.java
@@ -1,8 +1,14 @@
 package falusvampen.letsplay.service;
 
+import falusvampen.letsplay.exceptions.ProductCollectionException;
 import falusvampen.letsplay.models.Product;
 import falusvampen.letsplay.repositories.ProductRepository;
+import falusvampen.letsplay.repositories.UserRepository;
+import falusvampen.letsplay.models.User;
+import falusvampen.letsplay.config.ValidateProduct;
 import org.springframework.stereotype.Service;
+
+import javax.validation.ConstraintViolationException;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,13 +17,25 @@ import java.util.Optional;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final UserRepository userRepository;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, UserRepository userRepository) {
         this.productRepository = productRepository;
+        this.userRepository = userRepository;
     }
 
-    public Product createProduct(Product product) {
-        return productRepository.save(product);
+    public void createProduct(Product product) throws ConstraintViolationException, ProductCollectionException {
+        ValidateProduct.validateProduct(product);
+        if (product.getId() != null) {
+            product.setProductid(product.uuidGenerator());
+        }
+        Optional<User> userOptional = userRepository.findById(product.getUserid().trim());
+
+        if (userOptional.isEmpty()) {
+            throw new ProductCollectionException(ProductCollectionException.UserNotFoundException());
+        } else {
+            productRepository.save(product);
+        }
     }
 
     public List<Product> getAllProducts() {
@@ -28,23 +46,39 @@ public class ProductService {
         return productRepository.findById(id).orElse(null);
     }
 
-    public Product updateProduct(String id, Product updatedProduct) {
-        Optional<Product> optionalProduct = productRepository.findById(id);
-        if (optionalProduct.isPresent()) {
-            Product existingProduct = optionalProduct.get();
-            existingProduct.setName(updatedProduct.getName());
-            existingProduct.setPrice(updatedProduct.getPrice());
-            existingProduct.setDescription(updatedProduct.getDescription());
-            return productRepository.save(existingProduct);
+    public void updateProduct(String id, Product product) throws ProductCollectionException {
+        Optional<Product> productOptional = productRepository.findById(id);
+
+        ValidateProduct.validateProduct(product);
+
+        Optional<User> userOptional = userRepository.findById(product.getUserid().trim());
+
+        if (productOptional.isPresent()) {
+            if (productOptional.get().getName().equals(product.getName()) &&
+                    productOptional.get().getDescription().equals(product.getDescription()) &&
+                    productOptional.get().getPrice().equals(product.getPrice()) &&
+                    productOptional.get().getUserid().equals(product.getUserid())) {
+                throw new ProductCollectionException(ProductCollectionException.NoChangesMadeException());
+            } else if (userOptional.isEmpty()) {
+                throw new ProductCollectionException(ProductCollectionException.UserNotFoundException());
+            }
+            Product productUpdate = productOptional.get();
+            productUpdate.setName(product.getName());
+            productUpdate.setDescription(product.getDescription());
+            productUpdate.setPrice(product.getPrice());
+            productUpdate.setUserId(product.getUserid());
+            productRepository.save(productUpdate);
+        } else {
+            throw new ProductCollectionException(ProductCollectionException.NotFoundException(id));
         }
-        return null;
     }
 
-    public boolean deleteProduct(String id) {
-        if (productRepository.existsById(id)) {
+    public void deleteProduct(String id) throws ProductCollectionException {
+        Optional<Product> productOptional = productRepository.findById(id);
+        if (!productOptional.isPresent()) {
+            throw new ProductCollectionException(ProductCollectionException.NotFoundException(id));
+        } else {
             productRepository.deleteById(id);
-            return true;
         }
-        return false;
     }
 }
